c2c8a60394e29e6e939f1deace1a9220dd06266e
zyv42 <zyv42@sdf.org>
2020-01-28 16:47:57 +0200

    add deleting project functionality to the front-end

--- pma-frontend/src/actions/projectActions.js
+++ pma-frontend/src/actions/projectActions.js
@@ -1,40 +1,51 @@
 import axios from "axios";
-import {GET_ERRORS, GET_PROJECT, GET_PROJECTS} from "./types";
+import { GET_ERRORS, GET_PROJECT, GET_PROJECTS, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
     try {
-        const res = await axios.post("http://localhost:8080/api/project", project);
+        const res = await axios.post("/api/project", project);
         history.push("/dashboard");
         dispatch({
             type: GET_ERRORS,
             payload: {}
-        })
+        });
     } catch(error) {
         dispatch({
             type: GET_ERRORS,
             payload: error.response.data
-        })
+        });
     }
 };
 
 export const getProjects = () => async dispatch => {
-    const res = await axios.get("http://localhost:8080/api/project/all");
+    const res = await axios.get("/api/project/all");
     dispatch({
         type: GET_PROJECTS,
         payload: res.data
-    })
+    });
 };
 
 export const getProject = (id, history) => async dispatch => {
 
     try {
-        const res = await axios.get(`http://localhost:8080/api/project/${id}`)
+        const res = await axios.get(`/api/project/${id}`);
         dispatch({
             type: GET_PROJECT,
             payload: res.data
-        })
+        });
     } catch (error) {
         history.push("/dashboard");
     }
 
+};
+
+export const deleteProject = id => async dispatch => {
+
+    if (window.confirm("Are you sure you want to delete this entire project?")) {
+        await axios.delete(`/api/project/${id}`);
+        dispatch({
+            type: DELETE_PROJECT,
+            payload: id
+        });
+    }
 };
