9c37f2441d02d77b6ecd1c9c61f547e8b1e05cff
zyv42 <zyv42@sdf.org>
2020-01-29 20:00:15 +0200

    add routes to project board on front-end

--- pma-frontend/src/App.js
+++ pma-frontend/src/App.js
@@ -8,6 +8,8 @@ import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 class App extends Component {
   render() {
@@ -22,6 +24,10 @@ class App extends Component {
                            component={AddProject}/>
                     <Route exact path="/updateProject/:id"
                            component={UpdateProject}/>
+                    <Route exact path="projectBoard/:id"
+                           component={ProjectBoard}/>
+                    <Route exact path="/addProjectTask/:id"
+                           component={AddProjectTask}/>
                 </div>
             </Router>
           </Provider>
--- pma-frontend/src/components/ProjectBoard/ProjectBoard.js
+++ pma-frontend/src/components/ProjectBoard/ProjectBoard.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import Backlog from "./Backlog";
+import { Link } from "react-router-dom";
 
 class ProjectBoard extends Component {
     render() {
+        const { id } = this.props.match.params;
+
         return (
             <div className="container">
-                <a href=""
+                <Link to={`/addProjectTask/${id}`}
                    className="btn btn-primary mb-3">
                     <i className="fas fa-plus-circle">Create Project Task</i>
-                </a>
+                </Link>
                 <br />
                 <hr />
                 <Backlog />
