fce993681cc6b4bf625ef2c10d51215ddfdc698a
zyv42 <zyv42@sdf.org>
2020-02-09 19:11:01 +0200

    added user registration functionality to the front-end

--- pma-frontend/src/components/UserManagement/Register.js
+++ pma-frontend/src/components/UserManagement/Register.js
@@ -1,6 +1,41 @@
 import React, {Component} from 'react';
+import { createNewUser } from "../../actions/securityActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
 class Register extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            username: "",
+            fullName: "",
+            password: "",
+            confirmPassword: "",
+            errors: {}
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const newUser = {
+            username: this.state.username,
+            fullName: this.state.fullName,
+            password: this.state.password,
+            confirmPassword: this.state.confirmPassword
+        };
+
+        this.props.createNewUser(newUser, this.props.history);
+    }
+
+    onChange(e) {
+        this.setState({[e.target.name]:e.target.value})
+    }
+
     render() {
         return (
             <div className="register">
@@ -9,31 +44,39 @@ class Register extends Component {
                         <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Sign Up</h1>
                             <p className="lead text-center">Create your account</p>
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
                                     <input type="text"
                                            className="form-control form-control-lg"
-                                           placeholder="Name"
-                                           name="name"
+                                           placeholder="Fullname"
+                                           name="fullName"
+                                           value = {this.state.fullName}
+                                           onChange={this.onChange}
                                            required />
                                 </div>
                                 <div className="form-group">
                                     <input type="email"
                                            className="form-control form-control-lg"
-                                           placeholder="Email Address"
-                                           name="email" />
+                                           placeholder="Email Address (Username)"
+                                           value={this.state.username}
+                                           onChange={this.onChange}
+                                           name="username" />
                                 </div>
                                 <div className="form-group">
                                     <input type="password"
                                            className="form-control form-control-lg"
                                            placeholder="Password"
+                                           value={this.state.password}
+                                           onChange={this.onChange}
                                            name="password" />
                                 </div>
                                 <div className="form-group">
                                     <input type="password"
                                            className="form-control form-control-lg"
                                            placeholder="Confirm Password"
-                                           name="password2" />
+                                           value={this.state.confirmPassword}
+                                           onChange={this.onChange}
+                                           name="confirmPassword" />
                                 </div>
                                 <input type="submit"
                                        className="btn btn-info btn-block mt-4" />
@@ -46,4 +89,16 @@ class Register extends Component {
     }
 }
 
-export default Register;
+Register.propTypes = {
+    createNewUser: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps,
+    { createNewUser }
+)(Register);
