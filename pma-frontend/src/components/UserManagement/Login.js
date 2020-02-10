18d06ade7a5565c50e7e737398683fc0ee8bed4d
zyv42 <zyv42@sdf.org>
2020-02-10 17:26:00 +0200

    added and bound Login form

--- pma-frontend/src/components/UserManagement/Login.js
+++ pma-frontend/src/components/UserManagement/Login.js
@@ -1,6 +1,36 @@
 import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { login } from "../../actions/securityActions";
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: ""
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const LoginRequest = {
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        this.props.login(LoginRequest);
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
     render() {
         return (
             <div className="login">
@@ -8,17 +38,21 @@ class Login extends Component {
                     <div className="row">
                         <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Log In</h1>
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
                                     <input type="email"
                                            className="form-control form-control-lg"
                                            placeholder="Email Address"
-                                           name="email" />
+                                           value={this.state.username}
+                                           onChange={this.onChange}
+                                           name="username" />
                                 </div>
                                 <div className="form-group">
                                     <input type="password"
                                            className="form-control form-control-lg"
                                            placeholder="Password"
+                                           value={this.state.password}
+                                           onChange={this.onChange}
                                            name="password" />
                                 </div>
                                 <input type="submit"
@@ -32,4 +66,17 @@ class Login extends Component {
     }
 }
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    security: state.security,
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps,
+    {login}
+)(Login);
