5eff23618c5af99ddf59e7abb610f387f038cae1
zyv42 <zyv42@sdf.org>
2020-02-10 17:10:26 +0200

    added securityReducer and securityAction to start implementing login functionality to the front-end

--- pma-frontend/src/actions/securityActions.js
+++ pma-frontend/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import {GET_ERRORS} from "./types";
+import {GET_ERRORS, SET_CURRENT_USER} from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
     try {
@@ -13,6 +15,31 @@ export const createNewUser = (newUser, history) => async dispatch => {
         dispatch({
             type: GET_ERRORS,
             payload: error.response.data
+        });
+    }
+};
+
+export const login = LoginRequest => async dispatch => {
+    try {
+        // post => Login Request
+        const res = await axios.post("/api/users/login", LoginRequest);
+        // extract token from response data
+        const { token } = res.data;
+        // store the token in the localStorage
+        localStorage.setItem("jwtToken", token);
+        // set our token in the header
+        setJWTToken(token);
+        // decode token on React
+        const decoded = jwt_decode(token);
+        // dispatch to our securityReducer
+        dispatch({
+            type: SET_CURRENT_USER,
+            payload: decoded
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERRORS,
+            paylaod: error.response.data
         });
     }
 };
---
+++ pma-frontend/src/reducers/securityReducer.js
@@ -0,0 +1,23 @@
+import { SET_CURRENT_USER } from "../actions/types";
+
+const initialState = {
+    user: {},
+    validToken: false
+};
+
+const booleanActionPayload = (payload) => {
+    return !!payload;
+};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                validToken: booleanActionPayload(action.payload),
+                user: action.payload
+            };
+        default:
+            return state
+    }
+}
